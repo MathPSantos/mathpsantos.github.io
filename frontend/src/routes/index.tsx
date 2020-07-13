899dfc099ee2dd8e4f3970daacca1a345256fcc4
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-13 20:16:25 -0300

    feat: route.tsx started

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
