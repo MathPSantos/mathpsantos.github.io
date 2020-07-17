423905420327fd61898735f6e7a7cb3824fe29d3
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-16 21:55:31 -0300

    feat: components structure added

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,14 +1,45 @@
 import React from "react";
 
-import { Header } from "../../components";
+import { Layout } from "../../components";
 
-import { Container } from "./styles";
+import { 
+  Landing,
+  Content,
+  Greeting,
+  SocialList,
+  LinkedInIcon,
+  GithubIcon,
+  MailIcon,
+  DevToIcon,
+  LandingImg,
+} from "./styles";
+
+import LandingImage from '../../assets/images/LandingImage.png'
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <Layout>
+      <Landing>
+        <Content>
+          <Greeting>
+            <span>Hi. I'm Matheus,</span>
+            <h2>I’m a Developer <br/> and Designer</h2>
+
+            <SocialList>
+              <LinkedInIcon />
+              <GithubIcon />
+              <MailIcon/>
+              <DevToIcon />
+            </SocialList>
+          </Greeting>
+
+          {/* <Lang>
+
+          </Lang> */}
+        </Content>
+        <LandingImg src={LandingImage} />
+      </Landing>
+    </Layout>
   );
 };
 
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,9 +1,66 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Landing = styled.div`
     width: 100%;
-    min-height: 100vh;
-    
+    min-height: calc(100vh - 190px);
+
+    position: relative;
     display: flex;
-    overflow: hidden
+    align-items: center;
+
+    padding: 0 24px;
 `;
+
+export const Content = styled.div`
+    height: 100%;
+
+    background: ${({theme}) => theme.colors.background.primary};
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+export const Greeting = styled.div`
+    width: 100%;
+    height: 100%;
+    background: ${({theme}) => theme.colors.background.tertiary};
+
+    > span {
+        font-family: ${({theme}) => theme.fonts.openSansBold};
+        font-size: 12px;
+        text-transform: uppercase;
+        margin-top: auto;
+    };
+    > h2 {
+        font-size: 36px;
+        margin-bottom: auto;
+    };
+`;
+
+export const LandingImg = styled.img`
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    z-index: -3;
+
+    width: 300px;
+    height: auto;
+
+
+    @media (min-width: 760px) {
+        top: -90px;
+        width: 360px;
+    };
+`;
+
+export const SocialList = styled.div``;
+
+export const LinkedInIcon = styled.div``;
+
+export const GithubIcon = styled.div``;
+
+export const MailIcon = styled.div``;
+
+export const DevToIcon = styled.div``;
